64e510d07a815f8fa665ff6f809dca783e3dbe6f
Mphumza <mphumelelingema@gmail.com>
2024-09-13 10:55:18 +0200

    Commit azure changes

--- app.py
+++ app.py
@@ -38,6 +38,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    app.run(debug=os.environ.get('FLASK_DEBUG', False))
